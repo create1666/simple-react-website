45fbd9a581c3e406a64125fe817ab526f5fe3fa2
create1666 <create1666@gmail.com>
2021-05-27 21:00:24 +0100

    added the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaEarlybirds, FaTimes, FaBars } from "react-icons/fa";
-import Button from "../components/Button";
+import Button from "./Button";
+import "./Navbar.css";
+import "../App.css";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -27,44 +29,53 @@ const Navbar = () => {
     <>
       <div className="navbar">
         <div className="navbar-container container">
+          {/* flex-1 */}
           <Link to="/" className="navbar-logo-container">
-            <FaEarlybirds className="navbar-logo-icon" />
+            <FaEarlybirds className="navbar-logo" />
             CREATE1666
           </Link>
-          <div className="navabr-menu-icons" onClick={handleClickNavMenuIcon}>
+          {/* flex-2 */}
+          <div className="navabr-icon" onClick={handleClickNavMenuIcon}>
             {click ? <FaTimes /> : <FaBars />}
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links">
-                Products
-              </Link>
-            </li>
-            <li className="nav-btn">
-              {button ? (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline"> SIGN UP </Button>
+          {/* flex-3 */}{" "}
+          <div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              {/* ---children lists Starts here-- */}
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Home
                 </Link>
-              ) : (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
-                    {" "}
-                    SIGN UP{" "}
-                  </Button>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Services
                 </Link>
-              )}
-            </li>
-          </ul>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Products
+                </Link>
+              </li>
+
+              <li className="nav-btn">
+                {button ? (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline"> SIGN UP </Button>
+                  </Link>
+                ) : (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                      {" "}
+                      SIGN UP{" "}
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
